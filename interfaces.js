5d87251bcc9a4bd985de02a957fe453b88c5ada4
Ant Cosentino <antony@mycosentino.com>
2015-10-06 12:46:29 +0200

    hardened the awk regex selector - its still flaky but will allow alphanumeric interface names, in that order

--- interfaces.js
+++ interfaces.js
@@ -5,7 +5,7 @@ var child_process = require('child_process');
 
 module.exports = function(done) {
   child_process.exec(
-    'ifconfig -a | awk "/^[A-Za-z]+0:/ { print $1; }"',
+    'ifconfig -a | awk "/^[A-Za-z]+[0-9]+:/ { print $1; }"',
     function(err, stdout, stderr) {
       done(err, (stdout.toString().trim().split('\n') || []).map(function(iface) {
         return iface.split(':')[0];
