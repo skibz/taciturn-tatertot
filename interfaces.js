ed7a3594af6dba88b5252921aca3711660e3af45
Ant Cosentino <antony@mycosentino.com>
2015-10-09 13:29:59 +0200

    removed dependency on awk, using a far cleaner ifconfig command

--- interfaces.js
+++ interfaces.js
@@ -2,14 +2,28 @@
 'use strict';
 
 var child_process = require('child_process');
+var colours = ['red', 'green', 'blue', 'yellow', 'white', 'magenta', 'cyan'];
 
 module.exports = function(done) {
-  child_process.exec(
-    'ifconfig -a | awk "/^[A-Za-z]+[0-9]+:/ { print $1; }"',
-    function(err, stdout, stderr) {
-      done(err, (stdout.toString().trim().split('\n') || []).map(function(iface) {
-        return iface.split(':')[0];
-      }));
-    }
-  );
+  child_process.exec('ifconfig -lu', function(err, stdout, stderr) {
+    if (err) return done(err);
+    var ifaces = stdout.toString().trim().split(' ');
+    done(null, {
+      list: ifaces,
+      rx: ifaces.map(function(iface, i) {
+        return {
+          title: iface + ' rx',
+          style: {line: colours[i]},
+          x: [], y: []
+        };
+      }),
+      tx: ifaces.map(function(iface, i) {
+        return {
+          title: iface + ' tx',
+          style: {line: colours[i]},
+          x: [], y: []
+        };
+      })
+    });
+  });
 };
