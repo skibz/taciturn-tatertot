67ba8e2dbedff44e2d6dc4264519c51c3e4ca909
Ant Cosentino <antony@mycosentino.com>
2015-10-09 13:28:52 +0200

    added interfaces list to be passed into -i option for ifstat

--- ifstat.js
+++ ifstat.js
@@ -3,27 +3,27 @@
 
 var child_process = require('child_process');
 
-module.exports = function(update) {
+module.exports = function(ifaces, update) {
 
   var firstTick = true;
 
-  var ifstat = child_process.spawn('ifstat', ['-w', '-n']);
-  
+  var ifstat = child_process.spawn('ifstat', ['-w', '-n', '-i', ifaces.join(',')]);
+
   ifstat.stdout.on('data', function(data) {
     if (!firstTick) return update(data);
     firstTick = false;
   });
 
   ifstat.stderr.on('data', function(data) {
-    console.log('error line', data.toString());
+    console.log('ifstat error:', data.toString());
   });
 
   ifstat.on('close', function(code) {
-    console.log('ifstat closed with code', code);
+    console.log('ifstat closed with code:', code);
   }).on('exit', function(code, signal) {
-    console.log('ifstat exited with code and signal', code, signal);
+    console.log('ifstat exited with code and signal:', code, signal);
   }).on('error', function(err) {
-    console.log('ifstat error', err);
+    console.log('ifstat error:', err);
   });
-  
+
 };
