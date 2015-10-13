69f14a1f1df49a817c7715164ac3436eb7de270c
Ant Cosentino <antony@mycosentino.com>
2015-10-13 13:42:19 +0200

    moved chunking function call into ifstat module to do ticks with usage data

--- ifstat.js
+++ ifstat.js
@@ -1,14 +1,25 @@
 
 'use strict';
 
+var chunk = require('./chunk');
 var child_process = require('child_process');
 
-module.exports = function(ifaces, update) {
+module.exports = function(ifaces, tick) {
   var firstTick = true;
   var ifstat = child_process.spawn('ifstat', ['-w', '-n', '-i', ifaces.join(',')]);
 
   ifstat.stdout.on('data', function(data) {
-    if (!firstTick) return update(data);
+    if (!firstTick) {
+      return tick(
+        chunk.call(
+          data.toString().trim().split(
+            /\s/g
+          ).filter(function(line) {
+            return line !== '';
+          }), 2
+        )
+      );
+    }
     firstTick = false;
   });
 
