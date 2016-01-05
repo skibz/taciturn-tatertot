cfa6ae66e34686968c10dc597a8dc464abf4938d
Ant Cosentino <antony@mycosentino.com>
2016-01-05 11:54:03 +0200

    removed chunk and replaced with blow-chunks

--- chunk.js
+++
@@ -1,8 +0,0 @@
-
-'use strict';
-
-module.exports = function(size) {
-  return [].concat.apply([], this.map(function(element, index) {
-    return index % size ? [] : [this.slice(index, index + size)];
-  }, this));
-};
