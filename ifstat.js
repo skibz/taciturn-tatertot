4bf452ee1f0bc4173916ff88a25b1b53d014e5ca
Ant Cosentino <antony@mycosentino.com>
2016-01-05 11:56:11 +0200

    replace chunk with blow-chunks

--- ifstat.js
+++ ifstat.js
@@ -1,20 +1,19 @@
 
 'use strict';
 
-var chunk = require('./chunk');
+var chunk = require('blow-chunks');
 var child_process = require('child_process');
 
 module.exports = function(ifaces, tick) {
-  var firstTick = true;
-  var ifstat = child_process.spawn('ifstat', ['-w', '-n', '-i', ifaces.join(',')]);
+  var ifstat = child_process.spawn('ifstat', [
+    '-w', '-n', '-i', ifaces.join(',')
+  ]), firstTick = true;
 
   ifstat.stdout.on('data', function(data) {
     if (!firstTick) {
       return tick(
-        chunk.call(
-          data.toString().trim().split(
-            /\s/g
-          ).filter(function(line) {
+        chunk(
+          data.toString().trim().split(/\s/g).filter(function(line) {
             return line !== '';
           }), 2
         )
