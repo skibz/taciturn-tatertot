d32ec37fb71da11c46b3d5c51964c031cab2bf1c
Ant Cosentino <antony@mycosentino.com>
2016-01-05 11:54:11 +0200

    updated tests

--- test/index.js
+++ test/index.js
@@ -2,17 +2,8 @@
 'use strict';
 
 var expect = require('chai').expect;
-var chunk = require('../chunk');
 var interfaces = require('../interfaces');
 var ifstat = require('../ifstat');
-
-describe('chunk', function() {
-  it('should split an array into chunks of a given length', function() {
-    var input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    var expected = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]];
-    expect(chunk.call(input, 2)).to.deep.equal(expected);
-  });
-});
 
 describe('interfaces', function() {
   it('should return an object with specific keys', function(done) {
@@ -37,8 +28,6 @@ describe('interfaces', function() {
 describe('ifstat', function() {
   it('should invoke the function given as an argument', function(done) {
     this.timeout(5000);
-    ifstat(['lo0'], function() {
-      done();
-    });
+    ifstat(['lo0'], done.bind(done, null));
   });
 });
